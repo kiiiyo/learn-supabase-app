ed8b789607ffd439c75e1bfea315a723a1f7fdc3
kiiiyo <kiiiyo.jp@gmail.com>
2023-03-02 22:19:04 +0900

    feat: home page component

--- src/components/shared/header/header.component.tsx
+++ src/components/shared/header/header.component.tsx
@@ -1,5 +1,29 @@
+import Link from 'next/link';
+
 import * as styles from './styles';
 
 export function Header() {
-  return <header className={styles.root}>aaa</header>;
+  return (
+    <header className={styles.root}>
+      <div className={styles.inner}>
+        <div className="flex items-center justify-between">
+          <div className="flex items-center justify-start">
+            <Link href="/" className="flex ml-2 md:mr-24">
+              <span className="w-8 h-8 mr-3 text-3xl flex justify-center items-center">
+                👨‍💻
+              </span>
+              <span className="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">
+                Supabase App
+              </span>
+            </Link>
+          </div>
+          <div className="flex items-center">
+            <button className="inline-flex items-center text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center ml-3">
+              Sign in
+            </button>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 }
--- src/components/shared/header/styles.ts
+++ src/components/shared/header/styles.ts
@@ -1,3 +1,21 @@
-import { classnames, display, width } from 'tailwindcss-classnames';
+import {
+  backgroundColor,
+  borderColor,
+  borderWidth,
+  classnames,
+  padding,
+  position,
+  width,
+  zIndex
+} from 'tailwindcss-classnames';
 
-export const root = classnames(display('flex'), width('w-full'));
+export const root = classnames(
+  width('w-full'),
+  position('fixed'),
+  backgroundColor('bg-white'),
+  borderColor('border-b-gray-200'),
+  borderWidth('border-b'),
+  zIndex('z-30')
+);
+
+export const inner = classnames(padding('px-3', 'py-3', 'lg:px-5', 'lg:pl-3'));
